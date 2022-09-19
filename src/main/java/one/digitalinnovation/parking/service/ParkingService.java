155b950579c1be67f04399b8a8cdb610de7976d9
Lucas Teodoro <lucasteodoroac@gmail.com>
2022-09-19 19:53:58 -0300

    Erro ao implementar o Swagger

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -4,6 +4,7 @@ import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Array;
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,11 @@ public class ParkingService {
 
     static {
         var id = getUUID();
-        Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
+        var id1 = getUUID();
+        Parking parking = new Parking(id, "DMS-1111", "SC", "FORD CELTA", "PRETO");
+        Parking parking1 = new Parking(id1, "WAS-1234", "SP", "VW GOL", "VERMELHO");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll(){
@@ -24,5 +28,17 @@ public class ParkingService {
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public Parking findById(String id) {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
     }
 }
